03966ef764c0b980e647b7154846ac0d18f2a606
Nimb2 <egorbis@mail.ru>
2023-04-29 16:55:01 +0700

    Feat: LecturePage

--- src/pages/Lectures/index.tsx
+++ src/pages/Lectures/index.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import * as S from './styled';
+import { TitleWrapper } from './TitleWrapper';
+import { TagsWrapper } from './TagsWrapper';
+import { WrapperCards } from './WrapperCards';
 
 function LecturesPage() {
-  return <S.Wrapper>LecturesPage</S.Wrapper>;
+  return (
+    <S.Wrapper>
+      <TitleWrapper />
+      <TagsWrapper />
+      <WrapperCards />
+    </S.Wrapper>
+  );
 }
 
 export default LecturesPage;
