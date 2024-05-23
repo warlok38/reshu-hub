394e8f3f0919d4d41629c5fff56a03db38f3ba10
Yaroslav <warlok38@mail.ru>
2024-05-23 15:17:51 +0700

    chore: sticky header

--- src/widgets/header/ui/styled.ts
+++ src/widgets/header/ui/styled.ts
@@ -1,12 +1,19 @@
 import { styled } from '@mui/material';
 
 export const Wrapper = styled('div')`
+  position: sticky;
+  top: 0;
   display: flex;
   width: 100%;
   height: ${({ theme }) => theme.spacing(20)};
   padding: ${({ theme }) => theme.spacing(4)};
   box-shadow: 0px 0px 8px 0px rgba(34, 60, 80, 0.2);
-  z-index: 1;
+  background-color: ${({ theme }) => theme.palette.background.default};
+  z-index: 10;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.values.md}px) {
+    height: ${({ theme }) => theme.spacing(15)};
+  }
 `;
 
 export const Content = styled('div')`
