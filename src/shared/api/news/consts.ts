ca2375b2ea92d4f81474620e58a3c4fc7d032755
Mukhammadyusuf Ibrokhimov <myxamed12l@gmail.com>
2024-06-14 00:32:10 +0700

    improvement: implement invalidate list after mutations

--- src/shared/api/news/consts.ts
+++ src/shared/api/news/consts.ts
@@ -1,1 +1,3 @@
 export const NEWS_API = '/news';
+
+export const NEWS_TAG = 'News';
--- src/shared/api/news/newsApi.ts
+++ src/shared/api/news/newsApi.ts
@@ -1,45 +1,54 @@
 import { mainApi } from 'shared/api/http/mainApi';
 import { createQueryFn } from 'shared/api/http/queryFnBuilder';
 import { NewsEntity, NewsList } from 'shared/models/news';
-import { NEWS_API } from './consts';
+import { NEWS_API, NEWS_TAG } from './consts';
 
-export const newsApi = mainApi.injectEndpoints({
-  endpoints: (build) => ({
-    getNews: build.query<NewsList, void>({
-      queryFn: createQueryFn(
-        () => ({
+export const tags = [NEWS_TAG];
+
+export const newsApi = mainApi
+  // NOTE: see https://github.com/reduxjs/redux-toolkit/issues/1510
+  .enhanceEndpoints({ addTagTypes: tags })
+  .injectEndpoints({
+    endpoints: (build) => ({
+      getNews: build.query<NewsList, void>({
+        providesTags: tags,
+        queryFn: createQueryFn(
+          () => ({
+            url: NEWS_API,
+            method: 'GET',
+          }),
+          NewsList
+        ),
+      }),
+      getNewsDetail: build.query<NewsEntity, number>({
+        providesTags: tags,
+        queryFn: createQueryFn(
+          (id) => ({
+            url: `${NEWS_API}/${id}`,
+            method: 'GET',
+          }),
+          NewsEntity
+        ),
+      }),
+      createNews: build.mutation<void, FormData>({
+        invalidatesTags: tags,
+        queryFn: createQueryFn((data) => ({
           url: NEWS_API,
-          method: 'GET',
-        }),
-        NewsList
-      ),
-    }),
-    getNewsDetail: build.query<NewsEntity, number>({
-      queryFn: createQueryFn(
-        (id) => ({
+          method: 'POST',
+          data,
+        })),
+      }),
+      deleteNews: build.mutation<void, { id: number }>({
+        invalidatesTags: tags,
+        queryFn: createQueryFn(({ id }) => ({
           url: `${NEWS_API}/${id}`,
-          method: 'GET',
-        }),
-        NewsEntity
-      ),
+          method: 'DELETE',
+          data: id,
+        })),
+      }),
     }),
-    createNews: build.mutation<void, FormData>({
-      queryFn: createQueryFn((data) => ({
-        url: NEWS_API,
-        method: 'POST',
-        data,
-      })),
-    }),
-    deleteNews: build.mutation<void, { id: number }>({
-      queryFn: createQueryFn(({ id }) => ({
-        url: `${NEWS_API}/${id}`,
-        method: 'DELETE',
-        data: id,
-      })),
-    }),
-  }),
-  overrideExisting: false,
-});
+    overrideExisting: false,
+  });
 
 export const {
   useGetNewsQuery,
